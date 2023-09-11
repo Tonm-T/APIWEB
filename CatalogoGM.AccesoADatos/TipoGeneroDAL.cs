6af240b6ac0afaac3584594e9ccc9d88c62dbcc8
tony <tomy@gmail.com>
2023-09-11 16:51:15 -0600

    tipogenero DAL

--- CatalogoGM.AccesoADatos/TipoGeneroDAL.cs
+++ CatalogoGM.AccesoADatos/TipoGeneroDAL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CatalogoGM.EntidadesDeNegocio;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,82 @@ namespace CatalogoGM.AccesoADatos
 {
     public class TipoGeneroDAL
     {
+        public static async Task<int> CrearAsync(TipoGenero pTipoGenero)
+        {
+            int result = 0;
+            using (var dbcontext = new ComunDB())
+            {
+                dbcontext.Add<TipoGenero>(pTipoGenero);
+                result = await dbcontext.SaveChangesAsync();
+            }
+            return result;
+        }
+        public static async Task<int> ModificarAsync(TipoGenero pTipoGenero)
+        {
+            int result = 0;
+            using (var dbcontext = new ComunDB())
+            {
+                var tipogenero = await dbcontext.TipoGeneros.FirstOrDefaultAsync(s => s.Id == pTipoGenero.Id);
+                tipogenero.Tipo = pTipoGenero.Tipo;
+                dbcontext.Update(tipogenero);
+                result = await dbcontext.SaveChangesAsync();
+            }
+            return result;
+        }
+        public static async Task<int> EliminarAsync(TipoGenero pTipoGenero)
+        {
+            int result = 0;
+            using (var dbcontext = new ComunDB())
+            {
+                var tipogenero = await dbcontext.TipoGeneros.FirstOrDefaultAsync(s => s.Id == pTipoGenero.Id);
+                dbcontext.TipoGeneros.Remove(pTipoGenero);
+                result = await dbcontext.SaveChangesAsync();
+            }
+            return result;
+        }
+        public static async Task<TipoGenero> ObtenerPorIdAsync(TipoGenero pTipoGenero)
+        {
+            var tipogenero = new TipoGenero();
+            using (var dbcontext = new ComunDB())
+            {
+                tipogenero = await dbcontext.TipoGeneros.FirstOrDefaultAsync(s => s.Id.Equals(pTipoGenero.Id));
+            }
+            return tipogenero;
+        }
+        public static async Task<List<TipoGenero>> ObtenerTodosAsync(TipoGenero pTipoGenero)
+        {
+            var tipogeneros = new List<TipoGenero>();
+            using (var dbcontext = new ComunDB())
+            {
+                tipogeneros = await dbcontext.TipoGeneros.ToListAsync();
+            }
+            return tipogeneros;
+        }
+        internal static IQueryable<TipoGenero> QuerySelect(IQueryable<TipoGenero> pQuery, TipoGenero pTipoGenero)
+        {
+            if (pTipoGenero.Id > 0)
+                pQuery = pQuery.Where(s => s.Id == pTipoGenero.Id);
 
+            if (!string.IsNullOrWhiteSpace(pTipoGenero.Tipo))
+                pQuery = pQuery.Where(s => s.Tipo.Contains(pTipoGenero.Tipo));
+
+            pQuery = pQuery.OrderByDescending(s => s.Id).AsQueryable();
+
+            if (pTipoGenero.top_aux > 0)
+                pQuery = pQuery.Take(pTipoGenero.top_aux).AsQueryable();
+
+            return pQuery;
+        }
+        public static async Task<List<TipoGenero>> BuscarAsync(TipoGenero pTipoGenero)
+        {
+            var tipogenero = new List<TipoGenero>();
+            using (var bdContexto = new ComunDB())
+            {
+                var select = bdContexto.TipoGeneros.AsQueryable();
+                select = QuerySelect(select, pTipoGenero);
+                tipogenero = await select.ToListAsync();
+            }
+            return tipogenero;
+        }
     }
 }
